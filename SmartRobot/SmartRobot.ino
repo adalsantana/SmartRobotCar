4d8be43068b966b180a031149bd7f3fe11f93c8c
masterbum <asantan6@protonmail.com>
2019-05-25 21:54:09 -0400

    will be file containing all combined components of code written for elegoo car kit

---
+++ SmartRobot/SmartRobot.ino
@@ -0,0 +1,117 @@
+#include <IRremote.h>
+
+//------- IR REMOTE CODES ---------//
+#define FORWARD 16736925
+#define BACK    16754775
+#define LEFT    16720605
+#define RIGHT   16761405
+#define STOP    16712445
+
+#define RECV_PIN  12
+
+#define ENB 5   // Left  wheel speed
+#define IN1 7   // Left  wheel forward
+#define IN2 8   // Left  wheel reverse
+#define IN3 9   // Right wheel reverse
+#define IN4 11  // Right wheel forward
+#define ENA 6   // Right wheel speed
+#define carSpeed 200  // initial speed of car >=0 to <=255
+
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+unsigned long val;
+unsigned long preMillis;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  enableMotorDriver(); 
+  stop();
+  irrecv.enableIRIn();
+}
+
+void stop(){
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  Serial.println("STOP!");
+}
+
+
+void enableMotorDriver(){
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+}
+
+
+void forward() {
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("go forward!");
+}
+
+void back(){
+  digitalWrite(ENA, HIGH);
+  digitalWrite(ENB, HIGH);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("go back!");
+}
+
+void left(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH); 
+  Serial.println("go left!");
+}
+
+void right(){
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("go right!");
+}
+
+
+void recvIR(IRrecv *recvd){
+  if (irrecv.decode(&results)){ 
+    preMillis = millis();
+    val = results.value;
+    Serial.println(val);
+    irrecv.resume();
+    switch(val){
+      case FORWARD: forward();  break;
+      case BACK:    back();     break;
+      case LEFT:    left();     break;
+      case RIGHT:   right();    break;
+      case STOP:    stop();     break;
+      default:                  break;
+    }
+  }
+  else{
+    if(millis() - preMillis > 500){
+      stop();
+      preMillis = millis();
+    }
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
